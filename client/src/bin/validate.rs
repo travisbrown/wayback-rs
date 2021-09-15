3dbd7f65f2dc20e400794cf53ff910b92aba58c1
Travis Brown <travisrobertbrown@gmail.com>
2021-09-15 10:23:55 +0200

    Better validation

--- client/src/bin/validate.rs
+++ client/src/bin/validate.rs
@@ -1,38 +1,113 @@
+use clap::{crate_authors, crate_version, Clap};
+use futures::{lock::Mutex, FutureExt, StreamExt, TryFutureExt, TryStreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
+use simplelog::LevelFilter;
+use std::fs::File;
+use std::io::{LineWriter, Write};
 
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let files = std::fs::read_dir(&args[1])?.collect::<Vec<_>>();
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("I/O error")]
+    IOError(#[from] std::io::Error),
+    #[error("Tokio error: {0:?}")]
+    TaskError(#[from] tokio::task::JoinError),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let opts: Opts = Opts::parse();
+    let _ = init_logging(opts.verbose);
+
+    let output = File::create(opts.output)?;
+    let mut output = Mutex::new(LineWriter::new(output));
+
+    let files = std::fs::read_dir(opts.dir)?.collect::<Vec<_>>();
 
     let pb = ProgressBar::new(files.len() as u64);
     pb.set_style(ProgressStyle::default_bar()
         .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>7}/{len:7} ({eta})")
         .progress_chars("#>-"));
 
-    let mut invalid = vec![];
+    futures::stream::iter(files)
+        .map_err(Error::from)
+        .map_ok(|entry| {
+            let path = entry.path().clone();
+            let pb = pb.clone();
+            tokio::spawn(async move {
+                let digest = path.file_stem().unwrap().to_string_lossy();
+                let r = std::fs::File::open(&path)
+                    .and_then(|mut file| Ok(wayback_client::digest::compute_digest_gz(&mut file)?));
 
-    for result in files {
-        let path = result?.path();
+                let result = match r {
+                    Ok(value) if value == digest => None,
+                    Ok(value) => Some((path.into_boxed_path(), None)),
+                    Err(error) => Some((path.into_boxed_path(), Some(error))),
+                };
 
-        let digest = path.file_stem().unwrap().to_string_lossy();
-        let mut file = std::fs::File::open(&path)?;
+                pb.inc(1);
 
-        if wayback_client::digest::compute_digest_gz(&mut file)? != digest {
-            invalid.push(path.into_boxed_path());
-        }
+                let result: Result<Option<(Box<std::path::Path>, Option<std::io::Error>)>, Error> =
+                    Ok(result);
+                result
+            })
+            .err_into::<Error>()
+        })
+        .try_buffer_unordered(opts.parallelism)
+        .try_filter_map(|x| async { x })
+        .try_for_each(|result| async {
+            let message = match result {
+                (path, None) => format!("{}", path.display()),
+                (path, Some(error)) => format!("{},{:?}", path.display(), error),
+            };
 
-        pb.inc(1);
-    }
+            output
+                .lock()
+                .await
+                .write_all(format!("{}\n", message).as_bytes());
+            pb.println(message);
 
-    if invalid.is_empty() {
-        println!("All files are valid");
-    } else {
-        for path in invalid {
-            println!("{}", path.to_string_lossy());
-        }
-    }
+            Ok(())
+        })
+        .await?;
 
     Ok(())
+}
+
+#[derive(Clap)]
+#[clap(name = "validate", version = crate_version!(), author = crate_authors!())]
+struct Opts {
+    /// Level of verbosity
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    /// Level of parallelism
+    #[clap(short, long, default_value = "6")]
+    parallelism: usize,
+    /// Item directory
+    #[clap(short, long)]
+    dir: String,
+    /// Output file
+    #[clap(short, long)]
+    output: String,
+}
+
+fn select_log_level_filter(verbosity: i32) -> LevelFilter {
+    match verbosity {
+        0 => LevelFilter::Off,
+        1 => LevelFilter::Error,
+        2 => LevelFilter::Warn,
+        3 => LevelFilter::Info,
+        4 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
+    }
+}
+
+fn init_logging(verbosity: i32) -> Result<(), log::SetLoggerError> {
+    simplelog::TermLogger::init(
+        select_log_level_filter(verbosity),
+        simplelog::Config::default(),
+        simplelog::TerminalMode::Stderr,
+        simplelog::ColorChoice::Auto,
+    )
 }
 
 /*use futures::{TryFutureExt, TryStreamExt};
